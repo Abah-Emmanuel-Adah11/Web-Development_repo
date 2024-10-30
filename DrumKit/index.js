dbe69d7aa37a615af45c48746e2231f787c07403
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2024-10-30 11:04:55 -0700

    updates to .js file

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,10 +1,11 @@
-var numberOfDrumBottons  = document.querySelectorAll(".drum").length;
+let numberOfDrumBottons  = document.querySelectorAll(".drum").length;
 
 for(let i = 0; i < numberOfDrumBottons; i++){
 
 document.querySelectorAll(".drum")[i].addEventListener("click", function (){
 
      makeSound(this.innerHTML);
+     buttonAnimation(this.innerHTML);
 
 });
 
@@ -13,6 +14,7 @@ document.querySelectorAll(".drum")[i].addEventListener("click", function (){
 document.addEventListener("keypress", function(event){
 
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 
@@ -21,41 +23,52 @@ document.addEventListener("keypress", function(event){
     switch (key) {
 
         case "w":
-            var tom1 = new Audio("sounds/tom-1.mp3");
+            let tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
             break;
 
         case "a":
-            var tom2 = new Audio("sounds/tom-2.mp3");
+            let tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();
             break;
 
         case "s":
-            var tom3 = new Audio("sounds/tom-3.mp3");
+            let tom3 = new Audio("sounds/tom-3.mp3");
             tom3.play();
             break;
 
         case "d":
-            var tom4 = new Audio("sounds/tom-4.mp3");
+            let tom4 = new Audio("sounds/tom-4.mp3");
             tom4.play();
             break;
 
         case "j":
-            var snare = new Audio("sounds/snare.mp3");
+            let snare = new Audio("sounds/snare.mp3");
             snare.play();
             break;
 
         case "k":
-            var crash = new Audio("sounds/crash.mp3");
+            let crash = new Audio("sounds/crash.mp3");
             crash.play();
             break;
             
         case "l":
-            var kickBass = new Audio("sounds/kick-bass.mp3");
+            let kickBass = new Audio("sounds/kick-bass.mp3");
             kickBass.play();
             break;
     
         default: console.log(keyBoardKey);
             break;
-    }
+    } 
 }; 
+
+function buttonAnimation(currentKey){
+
+   let activeButton = document.querySelector("."+ currentKey);
+
+   activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },200);
+};
