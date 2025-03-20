63af275d4576332a1e681891309cbc6425aa00ac
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2025-03-20 13:45:20 -0800

    updates

--- DiceGame/index.js
+++ DiceGame/index.js
@@ -1,18 +1,18 @@
 /* image 1 */
-var randomNumber1 = Math.floor(Math.random()*6)+1; //Generating random numbers
+let randomNumber1 = Math.floor(Math.random()*6)+1; //Generating random numbers
 
-var image1 = "images/dice"+randomNumber1+".png";  //Obtaining our images base on the random number generated
+let image1 = "images/dice"+randomNumber1+".png";  //Obtaining our images base on the random number generated
 
-var image1Store = document.querySelectorAll("img")[0].setAttribute("src",image1); //changing images to match the random number
+let image1Store = document.querySelectorAll("img")[0].setAttribute("src",image1); //changing images to match the random number
 
 
 
 /* image 2 */
-var randomNumber2 = Math.floor(Math.random()*6)+1;
+let randomNumber2 = Math.floor(Math.random()*6)+1;
 
-var image2 = "images/dice"+randomNumber2+".png";
+let image2 = "images/dice"+randomNumber2+".png";
 
-var image2Store = document.querySelectorAll("img")[1].setAttribute("src", image2);
+let image2Store = document.querySelectorAll("img")[1].setAttribute("src", image2);
 
 
 
---
+++ PlayGround/index.js
@@ -0,0 +1,25 @@
+//Dice 1
+// Generating random numbers which will be multiplied by 6 and 1 added to cover up for the Math.floor rounding down.
+let random = Math.floor(Math.random() *6 + 1);
+
+// Dice 1 string concatinated for the dice image.
+let random_images = "images/dice"+random+".png";
+
+// it's "set attribute" method that set's the dice image from the default "src" to the random number generated.
+document.querySelector(".dice1").setAttribute("src", random_images);
+
+
+//DICE 2
+let random2 = Math.floor(Math.random() *6 + 1);
+let random_images2 = "images/dice"+random2+".png";
+
+document.querySelector(".dice2").setAttribute("src", random_images2)
+
+
+// Conditionals
+if (random > random2){
+    document.querySelector(".big_heading").innerHTML = "Player 1 won"
+}
+else if (random < random2){
+    
+}
